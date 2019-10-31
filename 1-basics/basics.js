64c30bc6f028fa637b854b6a1bd6e02c2f70bf64
Florence942 <taabuflo942@gmail.com>
2019-10-31 16:36:11 -0700

    js-exercises

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -51,7 +51,7 @@ describe("basics", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+           let somevariable =undefined;
             ////////////////////////////////////////////////////////////////////
 
             expect(typeof someVariable).to.equal("undefined");
